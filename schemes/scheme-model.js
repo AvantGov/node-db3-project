696233c798dfefa00d8636dc5bb19b98a0a11704
avantgov <alexcouts18@gmail.com>
2020-08-27 02:03:48 -0500

    wrote boiler plate to helper functions

---
+++ schemes/scheme-model.js
@@ -0,0 +1,44 @@
+// ! provide databse access in this file to write the helper functions
+// ! import this file to the router file if needed 
+
+const database_access = require("../data/config")
+
+const find = () => {
+
+
+}
+
+const findById = (id) => {
+
+
+}
+
+
+const findSteps = (id) => {
+
+
+}
+
+const add = (scheme) => {
+
+
+}
+
+const update = (changes, id) => {
+
+
+}
+
+const remove = (id) => {
+
+
+}
+
+module.exports = {
+    find,
+    findById,
+    findSteps,
+    add,
+    update,
+    remove
+}
