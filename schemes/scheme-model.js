673a58b31045da70ef43465084d14150784e071c
avantgov <alexcouts18@gmail.com>
2020-08-27 18:29:55 -0500

    MVP functionality implemented and tested

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -4,34 +4,43 @@
 const database_access = require("../data/config")
 
 const find = () => {
-
-
+    return database_access("schemes")
+        .select("scheme_name")
 }
 
 const findById = (id) => {
-
-
+    return database_access("schemes")
+        .select("scheme_name")
+        .where("id", id)
 }
 
 
 const findSteps = (id) => {
+    return database_access("schemes")
+        .innerJoin("steps", "steps.scheme_id", "schemes.id")
+        .select("scheme_name", "step_number", "instructions")
+        .where("schemes.id", id)
+        .orderBy("step_number")
 
 
 }
 
 const add = (scheme) => {
-
+    return database_access("schemes")
+        .insert(scheme)
 
 }
 
 const update = (changes, id) => {
-
-
+    return database_access("schemes")
+        .where("id", id)
+        .update(changes)
 }
 
 const remove = (id) => {
-
-
+    return database_access("schemes")
+        .where("id", id)
+        .del()
 }
 
 module.exports = {
